3b47340458faee0cafad0a37cb6bc735971d6d1d
Kelsey Judson <k+github@judson.nz>
2022-03-21 04:05:26 +1300

    Add clippy

--- .tmuxrc.sh
+++ .tmuxrc.sh
@@ -2,6 +2,7 @@ session_name=zero2prod
 
 init () {
 	tmux new-window -d -t $session_name -n check bacon check
+	tmux new-window -d -t $session_name -n clippy bacon clippy
 	tmux new-window -d -t $session_name -n test bacon test
 	tmux new-window -d -t $session_name -n run cargo watch -x run
 }
