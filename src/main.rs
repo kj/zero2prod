2a372b297823004c6fe34499761db1ec69efd5c8
Kelsey Judson <k+github@judson.nz>
2022-03-21 01:33:06 +1300

    Up to section 3.6

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("[::1]:8080")
+        .expect("Failed to bind to random port");
+    println!("Listening on [::1]:8080");
+    run(listener)?.await
 }
