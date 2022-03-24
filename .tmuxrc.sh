46eccd9e6d0e99e5176e229a3ebf381fbb09df01
Kelsey Judson <k+github@judson.nz>
2022-03-24 21:43:58 +1300

    Update tmux script

--- .tmuxrc.sh
+++ .tmuxrc.sh
@@ -1,7 +1,8 @@
 session_name=zero2prod
 
 init () {
-	tmux new-window -d -t $session_name -n test cargo watch -c -q -x 'test -q'
-	tmux new-window -d -t $session_name -n run cargo watch -c -q -x 'run -q'
-	tmux new-window -d -t $session_name -n clippy cargo watch -c -q -x 'clippy -q'
+	tmux new-window -d -t $session_name -n 'test' cargo watch -c -q -x 'test -q'
+	tmux new-window -d -t $session_name -n 'clippy' cargo watch -c -q -x 'clippy -q'
+	tmux new-window -d -t $session_name -n 'build' cargo watch -c -q -x 'build -q'
+	tmux new-window -d -t $session_name -n 'run' cargo watch -c --ignore-nothing -q -w target/debug/zero2prod -s ./target/debug/zero2prod
 }
