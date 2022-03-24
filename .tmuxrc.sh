26ce3592eb9e15454479cac4b442130ab47641ca
Kelsey Judson <k+github@judson.nz>
2022-03-24 21:12:15 +1300

    Default output is too noisy

--- .tmuxrc.sh
+++ .tmuxrc.sh
@@ -1,7 +1,7 @@
 session_name=zero2prod
 
 init () {
-	tmux new-window -d -t $session_name -n test cargo watch -c -x test
-	tmux new-window -d -t $session_name -n run cargo watch -c -x run
-	tmux new-window -d -t $session_name -n clippy cargo watch -c -x clippy
+	tmux new-window -d -t $session_name -n test cargo watch -c -q -x 'test -q'
+	tmux new-window -d -t $session_name -n run cargo watch -c -q -x 'run -q'
+	tmux new-window -d -t $session_name -n clippy cargo watch -c -q -x 'clippy -q'
 }
